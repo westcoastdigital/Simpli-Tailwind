c58a14132147088834f203494894f2edff39f10a
westcoastdigital <carassius@gmail.com>
2026-04-06 11:19:19 +0800

    Update: new helper classes

--- inc/helpers.php
+++ inc/helpers.php
@@ -306,6 +306,79 @@ if (!function_exists('acfimg')) {
     }
 }
 
+if (!function_exists('acfposts')) {
+    function acfposts(array $posts = [], string $post_type = 'post', int $min = 3, int $fallback_per_page = 10): array
+    {
+        // Ensure valid array of posts
+        $posts = is_array($posts) ? $posts : [];
+
+        // Filter out empty values (ACF sometimes returns false/null entries)
+        $posts = array_filter($posts);
+
+        $current_count = count($posts);
+
+        // If we already have enough, return early
+        if ($current_count >= $min) {
+            return $posts;
+        }
+
+        // Calculate how many we need
+        $needed = $min - $current_count;
+
+        // Get fallback posts (exclude already selected ones if possible)
+        $exclude_ids = array_map(function ($p) {
+            return is_object($p) ? $p->ID : (int) $p;
+        }, $posts);
+
+        $args = [
+            'post_type'      => $post_type,
+            'posts_per_page' => max($needed, $fallback_per_page),
+            'post_status'    => 'publish',
+            'post__not_in'   => $exclude_ids,
+            'orderby'        => 'date',
+            'order'          => 'DESC',
+        ];
+
+        $fallback = get_posts($args);
+
+        // Merge and trim to required minimum
+        $merged = array_merge($posts, $fallback);
+
+        return array_slice($merged, 0, $min);
+    }
+}
+
+if (!function_exists('acfbtn')) {
+    /**
+     * Get the ACF link field and convert to a button
+     *
+     * Returns a string
+     *
+     */
+    function acfbtn($field = [], $classes = '')
+    {
+
+        $content = '';
+
+        if (isarr($field)) {
+            $text = ifne($field, 'title');
+            $link = ifne($field, 'url');
+            $target = ifne($field, 'target');
+
+            if ($text != '' && $link != '') {
+                if ($target == '') {
+                    $target = '_self';
+                    $content = '<a href="' . $link . '" class="' . $classes . '" target="' . $target . '">' . $text . '</a>';
+                } else {
+                    $content = '<a href="' . $link . '" class="' . $classes . '">' . $text . '</a>';
+                }
+            }
+        }
+
+        return $content;
+    }
+}
+
 if (!function_exists('calculate_reading_time')) {
     /**
      * Calculate the average reading time for a post
@@ -421,4 +494,34 @@ if (!function_exists('simpli_breadcrumbs')) {
 
         return $breadcrumbs;
     }
+}
+
+function render_stars(float $rating): void
+{
+    for ($i = 1; $i <= 5; $i++) {
+        if ($rating >= $i) {
+            $icon = 'star';
+            $fill = 1;
+        } elseif ($rating >= $i - 0.5) {
+            $icon = 'star_half';
+            $fill = 1;
+        } else {
+            $icon = 'star';
+            $fill = 0;
+        }
+        echo '<span class="material-symbols-outlined text-3xl" style="font-variation-settings: \'FILL\' ' . $fill . ';">' . $icon . '</span>';
+    }
+}
+
+function sw_social_icons()
+{
+    $icons = [
+        'twitter' => '<svg width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M16.8198 20.7684L3.75317 3.96836C3.44664 3.57425 3.72749 3 4.22678 3H6.70655C6.8917 3 7.06649 3.08548 7.18016 3.23164L20.2468 20.0316C20.5534 20.4258 20.2725 21 19.7732 21H17.2935C17.1083 21 16.9335 20.9145 16.8198 20.7684Z" stroke="currentColor" stroke-width="1.5"/><path d="M20 3L4 21" stroke="currentColor" stroke-linecap="round"/></svg>',
+        'facebook' => '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"/></svg>',
+        'youtube' => '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"/></svg>',
+        'tiktok' => '<svg xmlns="http://www.w3.org/2000/svg" width="800" height="800" fill="currentColor" viewBox="0 0 512 512"><path d="M412.19 118.66a109 109 0 0 1-9.45-5.5 133 133 0 0 1-24.27-20.62c-18.1-20.71-24.86-41.72-27.35-56.43h.1C349.14 23.9 350 16 350.13 16h-82.44v318.78c0 4.28 0 8.51-.18 12.69 0 .52-.05 1-.08 1.56 0 .23 0 .47-.05.71v.18a70 70 0 0 1-35.22 55.56 68.8 68.8 0 0 1-34.11 9c-38.41 0-69.54-31.32-69.54-70s31.13-70 69.54-70a68.9 68.9 0 0 1 21.41 3.39l.1-83.94a153.14 153.14 0 0 0-118 34.52 161.8 161.8 0 0 0-35.3 43.53c-3.48 6-16.61 30.11-18.2 69.24-1 22.21 5.67 45.22 8.85 54.73v.2c2 5.6 9.75 24.71 22.38 40.82A167.5 167.5 0 0 0 115 470.66v-.2l.2.2c39.91 27.12 84.16 25.34 84.16 25.34 7.66-.31 33.32 0 62.46-13.81 32.32-15.31 50.72-38.12 50.72-38.12a158.5 158.5 0 0 0 27.64-45.93c7.46-19.61 9.95-43.13 9.95-52.53V176.49c1 .6 14.32 9.41 14.32 9.41s19.19 12.3 49.13 20.31c21.48 5.7 50.42 6.9 50.42 6.9v-81.84c-10.14 1.1-30.73-2.1-51.81-12.61"/></svg>',
+        'linkedin' => '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24"><path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z"/></svg>',
+    ];
+
+    return $icons;
 }
